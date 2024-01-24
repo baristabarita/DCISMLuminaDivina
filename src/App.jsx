12c77d7c8dd65f5c5647921ee64e0a0beae73dbb
BaristaBarita <18020776@usc.edu.ph>
2024-01-25 00:18:59 +0800

    added banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import Navbar from './components/navbar/navbar.jsx'
+import Banner from './components/banner/banner.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,23 +11,24 @@ function App() {
   return (
     <>
       <Navbar />
-      <div id="home" className='w-full mt-[6em]'>
-      </div>
-      <h1>Vite + React</h1>
-      <div id="about" className="card">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className='w-full animate-fade-in'>
+        <Banner/>
+        <h1>Vite + React</h1>
+        <div id="about" className="card">
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import bannerImg from '../../assets/banner2.png';
+
+const Banner = () => {
+    return (
+        <section className='banner relative h-screen flex items-center justify-center animate-fade-in' id="home">
+            <img
+                src={bannerImg}
+                alt="Banner"
+                className="object-cover w-full h-full"
+            />
+        </section>
+    );
+};
+
+export default Banner;
